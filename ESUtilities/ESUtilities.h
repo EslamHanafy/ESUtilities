60259ae9a7b4b20bd6f7892615aeb41e7a63d50a
Eslam <eslam@Eslams-iMac.local>
2017-08-04 00:31:08 +0200

    Initial Commit

---
+++ ESUtilities/ESUtilities.h
@@ -0,0 +1,19 @@
+//
+//  ESUtilities.h
+//  ESUtilities
+//
+//  Created by Eslam on 8/4/17.
+//  Copyright © 2017 magdsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESUtilities.
+FOUNDATION_EXPORT double ESUtilitiesVersionNumber;
+
+//! Project version string for ESUtilities.
+FOUNDATION_EXPORT const unsigned char ESUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESUtilities/PublicHeader.h>
+
+
